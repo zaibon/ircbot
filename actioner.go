25f805133700455c348fac3b3cfc94578fbbdeca
Christophe de Carvalho Pereira Martins <christophe.dcpm@gmail.com>
2015-01-02 20:30:53 +0100

    improve doc of Actioner

--- actioner.go
+++ actioner.go
@@ -3,9 +3,16 @@ package ircbot
 //ActionFunc is the type of function used in Actioner
 type ActionFunc func(b *IrcBot, m *IrcMsg)
 
-//Actionner is the interface that object need to implement custom action for the bot
+//Actioner is the interface that objects need to implement custom action for the bot
 type Actioner interface {
+	// array of IRC or user command on which the action will be triggered (MSG)
+	// example for IRC command: JOIN, PRIVMSG
+	// example for user command: .myCmd
 	Command() []string
+
+	// used to display help about the action
 	Usage() string
+
+	// actual action done by the bot
 	Do(b *IrcBot, m *IrcMsg)
 }
